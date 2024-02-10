8f9a189ecb1b782e67364c23ccfd0882ad60ca4a
charuka97 <charukatharindhu@gmail.com>
2024-02-10 16:46:02 +0530

    home page implementation done

--- src/pages/home/SpecialDishes.jsx
+++ src/pages/home/SpecialDishes.jsx
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Cards from "../../components/Cards";
+import {} from "react-icons/fa6";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
+const simpleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+    >
+      Next
+    </div>
+  );
+};
+
+const simplePreviewArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+    >
+      Back
+    </div>
+  );
+};
 
 const SpecialDishes = () => {
-    const [recipes, setRecipes] = useState([]);
-    const slider = React.useRef(null); 
+  const [recipes, setRecipes] = useState([]);
+  const slider = React.useRef(null);
+
+  useEffect(() => {
+    fetch("/menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const specials = data.filter((item) => item.category === "popular");
+        setRecipes(specials);
+      });
+  }, []);
 
   const settings = {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
       {
@@ -40,39 +76,40 @@ const SpecialDishes = () => {
         },
       },
     ],
+    nextArrow: <simpleNextArrow />,
+    previewArrow: <simplePreviewArrow />,
   };
   return (
-    <div className="section-container">
+    <div className="section-container my-20 relative">
       <div className="text-left">
         <p className="subtitle">Special Dishes</p>
         <h2 className="title md:w-[480px]">Standout Dishes From Our Menu</h2>
       </div>
 
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
+      <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        <button
+          onClick={() => slider?.current?.slickPrev()}
+          className="btn p-2 rounded-full ml-5"
+        >
+          <FaAngleLeft className="w-8 h-8 p-1" />
+        </button>
+
+        <button
+          onClick={() => slider?.current?.slickNext()}
+          className="btn p-2 rounded-full ml-5 bg-green"
+        >
+          <FaAngleRight className="w-8 h-8 p-1" />
+        </button>
+      </div>
+
+      <Slider
+        ref={slider}
+        {...settings}
+        className="overflow-hidden mt-10 space-x-5"
+      >
+        {recipes.map((item, i) => (
+          <Cards item={item} key={i} />
+        ))}
       </Slider>
     </div>
   );
