973373dbe4a3751398800e209da5f34375e3d13c
charuka97 <charukatharindhu@gmail.com>
2024-02-11 15:30:21 +0530

    product shop done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,44 +1,36 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Cards = ({ item }) => {
-  const [isHeartFileted, setIsHeartFileted] = useState(false);
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
 
   const handleHeartClick = () => {
-    setIsHeartFileted(!isHeartFileted);
+    setIsHeartFilled(!isHeartFilled);
   };
-
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div to={`/menu/${item._id}`} className="card shadow-xl relative mr-5 md:my-5">
       <div
-        className={`rating gap-1 absolute right-2 top-2 p-3 heartStar bg-green ${
-          isHeartFileted ? "text-rose-600" : "text-white"
+        className={`rating gap-1 absolute right-2 top-2 p-3.5 heartStar bg-green ${
+          isHeartFilled ? "text-rose-500" : "text-white"
         }`}
         onClick={handleHeartClick}
       >
-        <FaHeart className="h-5 w-5 cursor-pointer" />
+        <FaHeart className="w-3 h-3 cursor-pointer" />
       </div>
-      <Link to={`/menu/${item.id}`}>
+      <Link to={`/menu/${item._id}`}>
         <figure>
-          <img
-            src={item.image}
-            alt=""
-            className="hover:scale-105 transition-all duration-200 md:h-72"
-          />
+          <img src={item.image} alt="Shoes" className="hover:scale-105 transition-all duration-300 md:h-72" />
         </figure>
       </Link>
       <div className="card-body">
-        <Link to={`/menu/${item.id}`}>
-          <h2 className="card-title">{item.name}</h2>
-        </Link>
-        <p>Description on the items</p>
+       <Link to={`/menu/${item._id}`}><h2 className="card-title">{item.name}!</h2></Link>
+        <p>Description of the item</p>
         <div className="card-actions justify-between items-center mt-2">
-          <h5 className="text-semibold">
-            <span className="text-red text-sm text-semibold">$</span>
-            {item.price}
+          <h5 className="font-semibold">
+            <span className="text-sm text-red">$ </span> {item.price}
           </h5>
-          <button className="btn bg-green text-white">Buy Now</button>
+          <button className="btn bg-green text-white">Add to Cart </button>
         </div>
       </div>
     </div>
